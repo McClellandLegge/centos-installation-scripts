1ffd1287d38acd1222974fc95c42e192f2113743
McClelland Legge <McClelland.Legge@gmail.com>
2016-10-23 14:19:58 -0500

    git config and rstudio server

---
+++ git-config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git config --global user.email "McClelland.Legge@gmail.com"
+git config --global user.name "McClelland Legge"
+
---
+++ install-rstudio-server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo wget https://download2.rstudio.org/rstudio-server-rhel-0.99.903-x86_64.rpm
+sudo yum install -y --nogpgcheck rstudio-server-rhel-0.99.903-x86_64.rpm
